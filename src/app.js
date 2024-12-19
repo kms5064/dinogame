3fec42fa9e17767628edb44c2e60d6d2eb0c59b3
김제훈 <kms5064@naver.com>
2024-12-20 01:54:44 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
 
-server.listen(PORT, async () => {
+server.listen(PORT, '0.0.0.0', async () => {
   console.log(`Server is running on port ${PORT}`);
 
   try {
