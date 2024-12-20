306a0e39742ace0517f5776ae5492661c601a937
kimjehoon <kms5064@naver.com>
2024-12-20 09:07:04 +0900

    final

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { loadGameAssets } from './init/assets.js';
 const app = express();
 const server = createServer(app);
 
-const PORT = 3002;
+const PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
 
-server.listen(PORT, '0.0.0.0', async () => {
+server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 
   try {
